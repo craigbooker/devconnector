0c0b9f2d7c431898c3d62bff8d0d702e19ee8d23
Craig Booker <craig@arcadiasoftworks.com>
2020-03-17 20:51:57 -0500

    Playing catchup on the project files. Trying to figure out where I left off on this project.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
-// Reduxx
+import Routes from './components/routing/Routes';
+
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+const App = () => {
+	useEffect(() => {
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+			store.dispatch(loadUser());
+		}
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Switch>
+						<Route exact path='/' component={Landing} />
+						<Route component={Routes} />
+					</Switch>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
---
+++ client/src/actions/auth.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_PROFILE
+} from './types';
+
+// Load User
+export const loadUser = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
+
+// Register User
+
+// Login User
+
+// Logout / Clear Profile
