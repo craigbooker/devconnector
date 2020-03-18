ef585dc00d8adf3c828fec410556e4c5cbbad2f7
Craig Booker <craig@arcadiasoftworks.com>
2020-03-17 21:24:56 -0500

    Added some base layout files my project was missing.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,21 +1,70 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
-	try {
-		const res = await axios.get('/api/profile/me');
+  try {
+    const res = await axios.get('/api/profile/me');
 
-		dispatch({
-			type: GET_PROFILE,
-			payload: res.data
-		});
-	} catch (err) {
-		dispatch({
-			type: PROFILE_ERROR,
-			payload: { msg: err.response.statusText, status: err.response.status }
-		});
-	}
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
 };
+
+// Get all profiles
+
+// Get profile by ID
+
+// Get Github repos
+
+// Create or update profile
+
+// Add Experience
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.put('/api/profile/education', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Added', 'success'));
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add Education
+
+// Delete experience
+
+// Delete education
+
+// Delete account & profile
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -3,6 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../profile-forms/CreateProfile';
+import EditProfile from '../profile-forms/EditProfile';
+import AddExperience from '../profile-forms/AddExperience';
+import AddEducation from '../profile-forms/AddEducation';
 
 const Routes = () => {};
 
