8f491eb221a6c16d6e2d09479e2d3f607b04befd
Craig Booker <craig@arcadiasoftworks.com>
2020-03-17 21:02:36 -0500

    Added in some base components the project was missing in the profile-forms and profile.

---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+
+const Routes = () => {};
+
+export default Routes;
